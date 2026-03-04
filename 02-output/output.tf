d04d2e20b23c5ea5258dfb71e63099d1ae5d0c32
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-04 15:01:47 +0530

    output

--- 02-output/output.tf
+++ 02-output/output.tf
@@ -5,9 +5,13 @@ output "sample" {
 # any string value in terraform need to be quoted by double quotes
 # single quotes not supported
 
+# numeric and boolean (true,false)
 output "name" {
   value = 1000
 }
+
+
+# to aviod yes or no while running terraform apply use - terraform apply -auto-approve
 
 
   
