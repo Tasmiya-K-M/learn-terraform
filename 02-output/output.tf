61121d415760a0ddd71ce9badf161bff169384a4
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-04 14:57:04 +0530

    output

---
+++ 02-output/output.tf
@@ -0,0 +1,13 @@
+output "sample" {
+  value = "Hello World"
+}
+
+# any string value in terraform need to be quoted by double quotes
+# single quotes not supported
+
+output "name" {
+  value = 1000
+}
+
+
+  
