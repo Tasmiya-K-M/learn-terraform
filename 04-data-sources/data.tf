2770ccd1ae5c4a942234a29a10a009498dab7a8c
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-04 17:15:58 +0530

    datasources

---
+++ 04-data-sources/data.tf
@@ -0,0 +1,13 @@
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.medium"
+  availability_zone = "us-west-2a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
+}
+
+output "data-source-output" {
+  value = data.aws_ec2_spot_price.example.spot_price
+}
