41df79ff8364daeef99269c42f0c58a6d04a0193
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-04 16:08:44 +0530

    variables

--- 03-vars/variables.tf
+++ 03-vars/variables.tf
@@ -19,3 +19,51 @@ output "sample1" {
 output "sample1_variable_value" {
   value = "the value of sample1 - ${var.sample1}"
 }
+
+# variable Data types
+# 1. string
+# 2. number
+# 3. boolean
+
+# Variable Types in Ansible - 1.Plain  2.List  3.Map/Dict (Map means list of dict)
+
+# Variable Types in Terraform - 1.Plain  2.List  3.Map/Dict
+
+# Plain
+variable "course" {
+    default = "Devops Training"
+}
+
+# List
+variable "courses" {
+  default = ["Devops","AWS","Python"]
+}
+
+# Map
+variable "course_details" {
+  default = {
+    Devops = {
+        name = "Devops"
+        timings = "10am"
+        duration = 90
+    }
+    AWS = {
+        name = "AWS"
+        timings = "11am"
+        duration = 30
+    }
+  }
+}
+
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "course_details" {
+  value = var.course_details
+}
