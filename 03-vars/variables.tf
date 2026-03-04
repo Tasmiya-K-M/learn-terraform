441e2fd30ab86d3cea90300a623f233f3981a67b
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-04 15:13:54 +0530

    modified

--- 03-vars/variables.tf
+++ 03-vars/variables.tf
@@ -1,7 +1,12 @@
 variable "sample" {
-  default = 'hello world'
+  default = "hello world"
 }
 
 variable "sample1" {
     default = 1000
+}
+
+output "sample" {
+  value = var.sample
+  value = var.sample1
 }
