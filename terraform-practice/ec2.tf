c3021b33406b4b4b526aef1a545444fe5ebff0f6
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-11 12:28:23 +0530

    sg

--- terraform-practice/ec2.tf
+++ terraform-practice/ec2.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myec2" {
-  ami = "ami-02dfbd4ff395f2a1b"
-  instance_type = "t3.micro"
+# resource "aws_instance" "myec2" {
+#   ami = "ami-02dfbd4ff395f2a1b"
+#   instance_type = "t3.micro"
   
+# }
+
+resource "aws_security_group" "demo-security" {
+  name = "demo-security"
+  description = "terraform through"
 }
 
