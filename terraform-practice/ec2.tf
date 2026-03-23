33dad223b72057f657e380f949562c90961aa215
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-23 23:28:11 +0530

    eip

--- terraform-practice/ec2.tf
+++ terraform-practice/ec2.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "terraform-demo-security-group"
-  description = "created through terraform"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 100
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+resource "aws_eip" "lb" {
+  domain   = "vpc"
 }
