a393d91377ceb685d2b9152dc1c0fc6bdf3309bc
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-03-23 23:43:18 +0530

    eip

--- terraform-practice/ec2.tf
+++ terraform-practice/ec2.tf
@@ -1,19 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "sample-instance-terraform" {
-  ami = "ami-0b4f379183e5706b9"
-  instance_type = "t3.micro"
-}
-
-resource "aws_eip" "lb" {
-  domain   = "vpc"
-  instance = aws_instance.sample-instance-terraform.id
-}
-
-# resource "aws_eip_association" "eip_association" {
-#   instance_id = aws_instance.sample-instance-terraform.id
-#   allocation_id = aws_eip.lb.id
-# }
-
